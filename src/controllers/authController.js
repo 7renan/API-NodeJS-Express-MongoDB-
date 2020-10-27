54f281d796c815b14e32a0af6a8d60d306d5697c
unknown <lima.renan96@gmail.com>
2020-10-27 20:51:23 -0300

    router authenticate

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+
+// model
 const User = require('../models/User')
 
 
@@ -18,6 +21,20 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         return res.status(400).send({ error: 'Conection failed' })
     }
+})
+
+router.post('/authenticate', async (req, res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user)
+        return res.status(400).send({ error: 'User not found' })
+    
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'User not authenticate' })
+    
+    return res.send({ user })
 })
 
 module.exports = app => app.use('/auth', router)
